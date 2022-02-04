729fcba4976097c1dc47b10aeead765016017df0
Samir Maharjan <Samir84753@gmail.com>
2022-02-04 17:26:43 +0545

    Companydetail script and header files modified (#6)
    * test
    
    * added node setup
    
    * added script for listed companies
    
    * company detail added and header modification

--- scrape_scripts/listed_companies.py
+++ scrape_scripts/listed_companies.py
@@ -1,11 +1,11 @@
 from bs4 import BeautifulSoup
-from bs4 import NavigableString
 import requests
 import pandas as pd
+import json
 
 def scrape_listedcompanies():
     url='http://www.nepalstock.com/company/index/?_limit=400'
-    page=requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'})
+    page=requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36'})
     soup=BeautifulSoup(page.content,'html.parser')
     names=soup.select("tr.unique td")
     columnlist=[]
@@ -30,6 +30,7 @@ def scrape_listedcompanies():
         df = df.append(a_series, ignore_index=True)
 
     listedCompanies = df.to_json(orient = 'records')
-    # print(listedCompanies)
+    return listedCompanies
+    # print(type(listedCompanies))
 scrape_listedcompanies()
 
